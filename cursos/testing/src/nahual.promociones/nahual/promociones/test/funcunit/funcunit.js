cde3f634527b786701c4544d0d9085dfae537f18
Pablo D. Roca <pablodroca@gmail.com>
2011-09-05 00:28:00 +0000

    Funcunit eliminado para tener un sistema liviano. Se eliminan componentes java y test de integración de funcunit. Se mantienen archivos de qunit.
    git-svn-id: https://proyectonahual.svn.sourceforge.net/svnroot/proyectonahual@41 a13fab5c-0f33-4981-bb68-7b228bfc57fb

--- cursos/testing/src/nahual.promociones/nahual/promociones/test/funcunit/funcunit.js
+++
@@ -1,3 +0,0 @@
-steal
- .plugins("funcunit")
- .then("promociones_test");
--- cursos/testing/src/nahual.promociones/nahual/promociones/test/funcunit/promociones_test.js
+++
@@ -1,9 +0,0 @@
-module("promociones test", { 
-	setup: function(){
-		S.open("//nahual/promociones/promociones.html");
-	}
-});
-
-test("Copy Test", function(){
-	equals(S("h1").text(), "Welcome to JavaScriptMVC 3.0!","welcome text");
-});
