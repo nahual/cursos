9e907eb9e591f8281048c911dfa18a4856e3075f
Pablo D. Roca <pablodroca@gmail.com>
2011-11-05 03:15:33 +0000

    Copia del sistema para congelar version 3
    git-svn-id: https://proyectonahual.svn.sourceforge.net/svnroot/proyectonahual@60 a13fab5c-0f33-4981-bb68-7b228bfc57fb

---
+++ cursos/testing/src/nahual.promociones/nahual/promociones.v3/promociones.js
@@ -0,0 +1,42 @@
+steal.plugins(	
+	'jquery/controller',			// a widget factory
+	'jquery/controller/subscribe',	// subscribe to OpenAjax.hub
+	'jquery/view/ejs',				// client side templates
+	'jquery/controller/view',		// lookup views with the controller's name
+	'jquery/model',					// Ajax wrappers
+	'jquery/dom/fixture',			// simulated Ajax requests
+	'jquery/dom/form_params')		// form data helper
+	
+	.css(
+		'promociones', 
+		'resources/css/jquery-ui-1.8.16.custom'
+	)	// loads styles
+	.resources(
+		'jquery.nahual.js',
+		'external/jquery-ui-1.8.16.custom.min.js',
+		'external/jquery.validate.js'
+	)	// 3rd party script's (like jQueryUI), in resources folder
+
+	.models('promocion', 'usuario')		// loads files in models folder 
+
+	.controllers('base_controller', 'promocion', 'usuario')	// loads files in controllers folder
+
+	.views(
+		'promocion/edit.ejs',
+		'promocion/init.ejs',
+		'promocion/list.ejs',
+		'promocion/show.ejs',
+		'usuario/edit.ejs',
+		'usuario/init.ejs',
+		'usuario/list.ejs',
+		'usuario/show.ejs'
+	)
+	.then(function () {
+		$(document).ready(function () {
+			$('#tabs').tabs();
+			Nahual.Promociones.Controllers.BaseController.basePathVersionPostfix = '.v3';
+			new Nahual.Promociones.Controllers.Promocion($('#promocion'));
+			new Nahual.Promociones.Controllers.Usuario($('#usuario'));
+		});
+	}
+	);						// adds views to be added to build
