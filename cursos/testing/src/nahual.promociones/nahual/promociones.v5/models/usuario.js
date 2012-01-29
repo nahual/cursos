17925da1f85121162bfb1299a412fc0b5988144a
Pablo D. Roca <pablodroca@gmail.com>
2012-01-29 18:05:10 +0000

    git-svn-id: https://proyectonahual.svn.sourceforge.net/svnroot/proyectonahual@73 a13fab5c-0f33-4981-bb68-7b228bfc57fb

---
+++ cursos/testing/src/nahual.promociones/nahual/promociones.v5/models/usuario.js
@@ -0,0 +1,142 @@
+/**
+ * @tag models, home
+ * Wraps backend usuario services.  Enables 
+ * [Nahual.Promociones.Models.Usuario.static.findAll retrieving],
+ * [Nahual.Promociones.Models.Usuario.static.update updating],
+ * [Nahual.Promociones.Models.Usuario.static.destroy destroying], and
+ * [Nahual.Promociones.Models.Usuario.static.create creating] usuarios.
+ */
+$.Model.extend('Nahual.Promociones.Models.Usuario',
+/* @Static */
+{
+	editableAttributes: [
+		{
+			Name: 'cuenta',
+			Description: 'Cuenta',
+			ShowInList: true,
+			Required: true,
+			MaxLength: 50
+		},
+		{
+			Name: 'password',
+			Description: 'Password',
+			Type: 'password',
+			Required: true,
+			MaxLength: 16
+		},
+		{
+			Name: 'nombre',
+			Description: 'Nombre',
+			ShowInList: true,
+			Required: true
+		},
+		{
+			Name: 'apellido',
+			Description: 'Apellido', 
+			ShowInList: true
+		},
+		{
+			Name: 'rol',
+			Description: 'Rol',
+			ShowInList: true,
+			Required: true,
+			Type: 'select',
+			TypeValues: ['Usuario', 'Admin']
+		}
+	],
+	lastId: 0,
+	
+	/**
+ 	 * Retrieves usuarios data from your backend services.
+ 	 * @param {Object} params params that might refine your results.
+ 	 * @param {Function} success a callback function that returns wrapped usuario objects.
+ 	 * @param {Function} error a callback function for an error in the ajax request.
+ 	 */
+	findAll: function( params, success, error ){
+		$.ajax({
+			url: '/usuario',
+			type: 'get',
+			dataType: 'json',
+			data: params,
+			success: this.callback(success),
+			error: error,
+			fixture: function( settings, callbackType ) {
+				var modelObjects = [];
+				var usuarios = $.nahual.data.usuarios.getAll();
+				$.each(usuarios, function (index, value) {
+					modelObjects.push(new Nahual.Promociones.Models.Usuario(value));
+				});
+				return [modelObjects, 'success'];
+			} 
+		});
+	},
+	/**
+	 * Updates an usuario's data.
+	 * @param {String} id A unique id representing your usuario.
+	 * @param {Object} attrs Data to update your usuario with.
+	 * @param {Function} success a callback function that indicates a successful update.
+ 	 * @param {Function} error a callback that should be called with an object of errors.
+     */
+	update: function( id, attrs, success, error ){
+		$.ajax({
+			url: '/usuarios/'+id,
+			type: 'put',
+			dataType: 'json',
+			data: attrs,
+			success: success,
+			error: error,
+			fixture:  function( settings, callbackType ) {
+				$.nahual.data.usuarios.set(id, attrs);
+				var usuarios = $.nahual.data.usuarios.getAll();
+				return 'success';
+			} 
+		});
+	},
+	/**
+ 	 * Destroys an usuario's data.
+ 	 * @param {String} id A unique id representing your usuario.
+	 * @param {Function} success a callback function that indicates a successful destroy.
+ 	 * @param {Function} error a callback that should be called with an object of errors.
+	 */
+	destroy: function( id, success, error ){
+		$.ajax({
+			url: '/usuarios/'+id,
+			type: 'delete',
+			dataType: 'json',
+			success: success,
+			error: error,
+			fixture:  function( settings, callbackType ) {
+				var usuario = $.nahual.data.usuarios.del(id);
+				return (usuario ? 'sucess' : 'error');
+			} 
+		});
+	},
+	/**
+	 * Creates a usuario.
+	 * @param {Object} attrs A usuario's attributes.
+	 * @param {Function} success a callback function that indicates a successful create.  The data that comes back must have an ID property.
+	 * @param {Function} error a callback that should be called with an object of errors.
+	 */
+	create: function( attrs, success, error ){
+		$.ajax({
+			url: '/usuarios',
+			type: 'post',
+			dataType: 'json',
+			success: success,
+			error: error,
+			data: attrs,
+			fixture: function( settings, callbackType ) {
+				var id = $.nahual.data.usuarios.getLatestId() + 1;
+				$.nahual.data.usuarios.set(id, attrs);
+				var usuarios = $.nahual.data.usuarios.getAll();
+				return 'success';
+			} 
+		});
+	}
+},
+/* @Prototype */
+{
+	nombreCompleto: function (){
+		return this.nombre + " " + this.apellido;
+	}
+});
---
+++ cursos/testing/src/nahual.promociones/nahual/promociones.v5/promociones.js
@@ -0,0 +1,57 @@
+steal.plugins(	
+	'jquery/controller',			// a widget factory
+	'jquery/controller/subscribe',	// subscribe to OpenAjax.hub
+	'jquery/view/ejs',				// client side templates
+	'jquery/controller/view',		// lookup views with the controller's name
+	'jquery/model',					// Ajax wrappers
+	'jquery/dom/fixture',			// simulated Ajax requests
+	'jquery/dom/form_params')		// form data helper
+	
+	.css(
+		'promociones', 
+		'resources/css/jquery-ui-1.8.16.custom'
+	)	// loads styles
+	.resources(
+		'jquery.nahual.js',
+		'external/jquery-ui-1.8.16.custom.min.js',
+		'external/jquery.validate.js'
+	)	// 3rd party script's (like jQueryUI), in resources folder
+
+	.models('promocion', 'publicacion', 'usuario')		// loads files in models folder 
+
+	.controllers('base_controller', 'promocion', 'publicacion', 'usuario', 'login')	// loads files in controllers folder
+
+	.views(
+		'promocion/edit.ejs',
+		'promocion/init.ejs',
+		'promocion/list.ejs',
+		'promocion/show.ejs',
+		'publicacion/edit.ejs',
+		'publicacion/init.ejs',
+		'publicacion/list.ejs',
+		'publicacion/show.ejs',
+		'usuario/edit.ejs',
+		'usuario/init.ejs',
+		'usuario/list.ejs',
+		'usuario/show.ejs',
+		'login/init.ejs'
+	)
+	.then(function () {
+		$(document).ready(function () {
+			Nahual.Promociones.Controllers.BaseController.basePathVersionPostfix = '';
+			var promo = new Nahual.Promociones.Controllers.Promocion($('#promocion'));
+			var publicacion = new Nahual.Promociones.Controllers.Publicacion($('#publicacion'));
+			var usuario = new Nahual.Promociones.Controllers.Usuario($('#usuario'));
+			new Nahual.Promociones.Controllers.Login($('#login'));
+			$('#tabs').tabs({
+				select: function (event, ui) {
+					var allowSelect = !(promo.inEditMode || usuario.inEditMode || publicacion.inEditMode);
+					if (!allowSelect){
+						alert("Debe finalizar la edición antes de cambiar de sección");
+					}
+					return allowSelect;
+				}
+			});
+		});
+	}
+	);						// adds views to be added to build
